742b98fa1f89be68b00614041c25645fd17d20e7
Alvin Lucillo <allouothers@gmail.com>
2025-02-11 20:38:06 +0800

    Create basic user test

---
+++ gobddintgtest/integration_test/users_test.go
@@ -0,0 +1,35 @@
+package integration_test
+
+import (
+	"context"
+	"gobddintgtest/integration_test/utils"
+	"gobddintgtest/internal/models"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetUsers(t *testing.T) {
+	// Given
+	var users []interface{}
+	users = append(users, models.User{
+		Name: "John Doe",
+	})
+	users = append(users, models.User{
+		Name: "Jane Doe",
+	})
+	if err := utils.InsertUsers(context.TODO(), users); err != nil {
+		require.NoError(t, err)
+	}
+
+	// When
+	result, err := userService.GetUsers(context.TODO())
+
+	// Then
+	require.NoError(t, err)
+	require.NotEmpty(t, users)
+	require.Equal(t, len(users), len(result))
+	for i := range users {
+		require.Equal(t, users[i].(models.User).Name, result[i].Name)
+	}
+}
