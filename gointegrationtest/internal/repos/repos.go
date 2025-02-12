b7fbd149e88713b7020ed7b0b7664c57c59bbed6
Alvin Lucillo <allouothers@gmail.com>
2025-02-12 21:00:48 +0800

    Use db constant

--- gointegrationtest/internal/repos/repos.go
+++ gointegrationtest/internal/repos/repos.go
@@ -1,13 +1,17 @@
 package repos
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"gointegrationtest/internal/database"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type RepoCollection struct {
 	User UserRepo
 }
 
 func NewRepoCollection(client *mongo.Client) RepoCollection {
-	db := client.Database("gobdddb")
+	db := client.Database(database.DB_NAME)
 
 	return RepoCollection{
 		User: NewUserRepo(db),
