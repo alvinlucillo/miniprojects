497f9de136f25beb9444012df9013f0b00a164fc
Alvin Lucillo <allouothers@gmail.com>
2025-02-16 20:20:15 +0800

    Update batch repo to db generation

--- gointegrationtest/internal/repos/batch.go
+++ gointegrationtest/internal/repos/batch.go
@@ -7,52 +7,43 @@ import (
 	"gointegrationtest/internal/models"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type BatchRepo struct {
-	coll *mongo.Collection
+	exportedDBColl *mongo.Collection
 }
 
 func NewBatchRepo(db *mongo.Database) BatchRepo {
 	return BatchRepo{
-		db.Collection(database.BATCH_COLLECTION),
+		exportedDBColl: db.Collection(database.EXPORTED_DB_COLLECTION),
 	}
 }
 
-func (b BatchRepo) GetBatches(ctx context.Context) ([]models.Batch, error) {
-	var batches []models.Batch
+func (b BatchRepo) GetDBExports(ctx context.Context) ([]models.DBExport, error) {
+	var dbExports []models.DBExport
 
-	if err := database.FindAll(b.coll, &batches); err != nil {
+	if err := database.FindAll(b.exportedDBColl, &dbExports); err != nil {
 		return nil, fmt.Errorf("failed to get batches: %w", err)
 	}
-	return batches, nil
+	return dbExports, nil
 }
 
-func (b BatchRepo) InsertBatch(ctx context.Context, batch models.Batch) (string, error) {
-	result, err := b.coll.InsertOne(ctx, batch)
+func (b BatchRepo) InsertDBExports(ctx context.Context, dbExport models.DBExport) (primitive.ObjectID, error) {
+	result, err := b.exportedDBColl.InsertOne(ctx, dbExport)
 	if err != nil {
-		return "", fmt.Errorf("failed to insert batch: %w", err)
+		return primitive.NilObjectID, fmt.Errorf("failed to insert dbExport: %w", err)
 	}
 
-	id := result.InsertedID.(string)
+	id := result.InsertedID.(primitive.ObjectID)
 	return id, nil
 }
 
-func (b BatchRepo) UpdateBatch(ctx context.Context, batch models.Batch) error {
-	_, err := b.coll.UpdateOne(ctx, bson.M{"_id": batch.ID}, bson.M{"$set": batch})
+func (b BatchRepo) UpdateDBExport(ctx context.Context, dbExport models.DBExport) error {
+	_, err := b.exportedDBColl.UpdateOne(ctx, bson.M{"_id": dbExport.ID}, bson.M{"$set": dbExport})
 	if err != nil {
-		return fmt.Errorf("failed to update batch: %w", err)
+		return fmt.Errorf("failed to update dbExport: %w", err)
 	}
 	return nil
-}
-
-func (b BatchRepo) GetBatch(ctx context.Context, id string) (models.Batch, error) {
-	var batch models.Batch
-
-	err := b.coll.FindOne(ctx, bson.M{"_id": id}).Decode(&batch)
-	if err != nil {
-		return models.Batch{}, fmt.Errorf("failed to get batch: %w", err)
-	}
-	return batch, nil
 }
