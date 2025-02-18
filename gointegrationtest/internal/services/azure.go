3559e63522fa468e43ddcdea3486831fd797bb5d
Alvin Lucillo <allouothers@gmail.com>
2025-02-18 20:46:27 +0800

    Get blob file

--- gointegrationtest/internal/services/azure.go
+++ gointegrationtest/internal/services/azure.go
@@ -74,3 +74,18 @@ func (a AzureManager) ListBlobs() error {
 
 	return nil
 }
+
+func (a AzureManager) GetBlobFile(ctx context.Context, blobName, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("creating file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = a.client.DownloadFile(ctx, a.containerName, blobName, file, nil)
+	if err != nil {
+		return fmt.Errorf("downloading blob to file: %w", err)
+	}
+
+	return nil
+}
